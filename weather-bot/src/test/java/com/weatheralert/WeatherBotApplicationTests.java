93fc3b0e1a25e280232ecfba2329afd89fe10369
BusyHand <88427170+BusyHand@users.noreply.github.com>
2023-04-02 22:45:18 +0500

    Migrate from Postgresql to H2 memory
    Because Heroku turned off free access to db

--- weather-bot/src/test/java/com/weatheralert/WeatherBotApplicationTests.java
+++ weather-bot/src/test/java/com/weatheralert/WeatherBotApplicationTests.java
@@ -1,20 +1,5 @@
 package com.weatheralert;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.junit.jupiter.api.Test;
-
 class WeatherBotApplicationTests {
 
-	@Test
-	void contextLoads() {
-
-		Pattern pattern = Pattern.compile("\\d?\\d:\\d\\d?");
-		Matcher matcher1 = pattern.matcher("лфыовдлфы    0:1     длвтатыф");
-
-		if (matcher1.find()) {
-			System.out.println(matcher1.group());
-		}
-	}
 }
